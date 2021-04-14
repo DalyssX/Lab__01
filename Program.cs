bdc6f2e5ecea79473fb5bdde092affc17d8fd7ae
DalyssX <82532054+DalyssX@users.noreply.github.com>
2021-04-14 19:56:12 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Lab__01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
